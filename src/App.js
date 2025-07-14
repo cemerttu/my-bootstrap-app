4f684ae2f17e09858f917e43140df311dcaf7be4
ian <cemenrttu@gmail.com>
2025-07-14 10:14:17 +0300

    firstcommit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Navbar, Container, Card, Row, Col } from 'react-bootstrap';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+        {/* Navbar */}
+        <Navbar bg="dark" variant="dark">
+          <Container>
+            <Navbar.Brand href="#">My React-Bootstrap App</Navbar.Brand>
+          </Container>
+        </Navbar>
+
+        {/* Heading */}
+        <Container className="mt-4">
+          <h1 className="text-center">Welcome to My App</h1>
+
+          {/* Cards */}
+          <Row className="mt-4">
+            <Col md={4}>
+              <Card>
+                <Card.Body>
+                  <Card.Title>Card 1</Card.Title>
+                  <Card.Text>This is the content of card 1.</Card.Text>
+                </Card.Body>
+              </Card>
+            </Col>
+
+            <Col md={4}>
+              <Card>
+                <Card.Body>
+                  <Card.Title>Card 2</Card.Title>
+                  <Card.Text>This is the content of card 2.</Card.Text>
+                </Card.Body>
+              </Card>
+            </Col>
+
+            <Col md={4}>
+              <Card>
+                <Card.Body>
+                  <Card.Title>Card 3</Card.Title>
+                  <Card.Text>This is the content of card 3.</Card.Text>
+                </Card.Body>
+              </Card>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    </>
   );
 }
 
